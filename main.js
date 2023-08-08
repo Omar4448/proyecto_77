62085ef7e7a79a24ec0deb604d237fd68c94a371
Omar4448 <129348686+Omar4448@users.noreply.github.com>
2023-08-08 12:08:35 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var nombre1="carro"
+var nombre2="excavadora voladora militar"
+
+var numero1=5
+var numero2=123
+
+var suma=numero1+numero2
+console.log(suma)
+var concatenar=nombre1+nombre2
+console.log(concatenar)
